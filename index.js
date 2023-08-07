fec3a0b20a6e2e9f5bbf995b2b986c790bda421b
Poteau Florian <florianpoteau59@gmail.com>
2023-08-07 15:42:13 +0200

    add: carousel pour projet réalisé

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const carouselSlide = document.querySelector(".carousel-slide");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
 
 let translateValue = 0;
 
@@ -27,3 +27,35 @@ prevBtn.addEventListener("click", () => {
 
   carouselSlide.style.transform = `translateX(${translateValue}px)`;
 });
+
+
+
+
+const carouselSlide1 = document.querySelector(".carousel-slide1");
+const prevBtn1 = document.querySelector(".prevBtn1");
+const nextBtn1 = document.querySelector(".nextBtn1");
+
+let currentIndex = 0;
+
+nextBtn1.addEventListener("click", () => {
+  const slideWidth = carouselSlide1.clientWidth;
+  const totalSlides = carouselSlide1.childElementCount;
+
+  // Calculate the new translateValue based on the current index
+  currentIndex = (currentIndex + 1) % totalSlides;
+  const translateValue1 = -(slideWidth * currentIndex);
+
+  carouselSlide1.style.transform = `translateX(${translateValue1}px)`;
+});
+
+prevBtn1.addEventListener("click", () => {
+  const slideWidth = carouselSlide1.clientWidth;
+  const totalSlides = carouselSlide1.childElementCount;
+
+  // Calculate the new translateValue based on the current index
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  const translateValue1 = -(slideWidth * currentIndex);
+
+  carouselSlide1.style.transform = `translateX(${translateValue1}px)`;
+});
+
