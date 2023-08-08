47308b18e5e9a9a36e312bbee0d4b7e71b8bcef5
Poteau Florian <florianpoteau59@gmail.com>
2023-08-08 11:21:37 +0200

    add: formulaire

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+
+
 const carouselSlide = document.querySelector(".carousel-slide");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
